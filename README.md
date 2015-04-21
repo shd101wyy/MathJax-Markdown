94c9813ee6609de74ed135a16320f0234f9a27c8
Yiyi Wang <shd101wyy@sina.com>
2015-04-20 20:32:07 -0500

    first push

---
+++ README.md
@@ -0,0 +1,10 @@
+### Preview Markdown with MathJax support
+** Inline Math Support using MathJax **
+- Put your math expression within $\$...\$$
+- eg $\$$x_1 + 2$\$$ will be render as $x_1 + 2$
+
+
+### Implementation.
+- I used ** [marked](https://github.com/chjj/marked) ** to convert markdown code to html code.
+- I used ** [MathJax](https://www.mathjax.org/)** to render math expression in * LaTex * style.
+- I used ** [JsPDF](https://github.com/MrRio/jsPDF) ** to compile rendered html to pdf file.
---
+++ mathjax_markdown.js
@@ -0,0 +1,79 @@
+//var markdown = require("markdown").markdown;
+var marked = require('marked');
+var fs = require("fs");
+
+var mathjax_markdown = {};
+
+/**
+ *
+ * Load markdown content
+ * compile it to html content
+ * @param  {[string]} input_md_content input markdown content
+ * @param  {[boolean]} use_mathjax      whether to use mathjax
+ * @return {[string]}                  generated html content
+ */
+ mathjax_markdown.html = function(input_md_content, use_mathjax){
+    if (use_mathjax === void 0)
+        use_mathjax = true;
+    var html = marked(input_md_content);
+    if (use_mathjax){
+        html += '\n<script type="text/x-mathjax-config"> MathJax.Hub.Config({tex2jax: {inlineMath: [[\'$\',\'$\'], [\'\\\\(\',\'\\\\)\']]}}); </script>';
+
+        // cdn
+        html += '<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"> </script>';
+
+        // local
+        //html += '<script type="text/javascript" src="./MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML"> </script>';
+    }
+    return html;
+};
+/**
+ * Load markdown content, generate pdf
+ * @param  {string} input_md_content       [description]
+ * @param  {boolean} use_mathjax           [description]
+ * @param  {string} generate_pdf_filename  [description]
+ * @return {[type]}                        [description]
+ */
+ mathjax_markdown.pdf = function(input_md_content, generate_pdf_filename, use_mathjax){
+    if (generate_pdf_filename === void 0){
+        generate_pdf_filename = "./preview.pdf";
+    }
+    var html = mathjax_markdown.html(input_md_content, use_mathjax);
+};
+
+/**
+ * Given input md file, output html file
+ */
+mathjax_markdown.md_to_html = function(md_file_name, html_file_name, use_mathjax){
+    if (use_mathjax === void 0)
+        use_mathjax = true;
+
+    var md_content = fs.readFileSync(md_file_name, 'utf8');
+    var html_content = mathjax_markdown.html(md_content, use_mathjax);
+
+    fs.writeFile(html_file_name, html_content, function(err){
+        if (err){
+            console.log(err);
+        }
+    });
+};
+
+
+/**
+ * Given input md file, output pdf file
+ * jsPDF doesn't have nodejs port
+ */
+mathjax_markdown.md_to_pdf = function(md_file_name, pdf_file_name, use_mathjax){
+    console.log("not implemented yet");
+};
+/*
+mathjax_markdown.md_to_pdf = function(md_file_name, pdf_file_name, use_mathjax){
+    if (use_mathjax === void 0)
+        use_mathjax = true;
+
+    var md_content = fs.readFileSync(md_file_name, 'utf8');
+    mathjax_markdown.pdf(md_content, pdf_file_name, use_mathjax);
+};
+*/
+if (typeof(module) !== "undefined")
+    module.exports = mathjax_markdown;
